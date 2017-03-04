45b9b56a0406159e68fac1f22053aa62884a0b80
uwol <u.wol@wwu.de>
2017-03-04 17:23:13 +0100

    optimizations

--- network/preprocess.py
+++ network/preprocess.py
@@ -1,5 +1,5 @@
 import os
-import cStringIO
+from PIL import Image
 import tensorflow as tf
 from model import decode_jpeg, WIDTH, HEIGHT, CHANNELS
 
@@ -10,6 +10,7 @@ TARGET_DIR = "/home/models/art/preprocessed"
 TRAIN_EXTENSION = "train"
 VALIDATION_EXTENSION = "validation"
 VALIDATION_MOD = 100
+FILE_MIN_SIZE_KB = 100
 
 
 def process_artist(artist_dir, artist_number):
@@ -25,36 +26,45 @@ def process_artist(artist_dir, artist_number):
       image_path = os.path.join(artist_path, image_file)
 
       if os.path.isfile(image_path) and image_path.lower().endswith(("jpg", "jpeg")):
-        if image_number > 0 and image_number % VALIDATION_MOD == 0:
-          extension = VALIDATION_EXTENSION
-        else:
-          extension = TRAIN_EXTENSION
+        filesize = os.path.getsize(image_path)
 
-        with file(image_path) as f:
-          image = f.read()
-          filename = os.path.basename(image_path)
-          write_artist_tfrecord(artist_dir, artist_number, image_number, filename, extension, image)
-          image_number += 1
+        if filesize > FILE_MIN_SIZE_KB * 1000:
+          image = Image.open(image_path)
+          width, height = image.size
 
-    return True
+          if width >= WIDTH and height >= HEIGHT:
+            filename = os.path.basename(image_path)
 
-def write_artist_tfrecord(artist_dir, artist_number, image_number, filename, extension, image):
+            if image_number > 0 and image_number % VALIDATION_MOD == 0:
+              extension = VALIDATION_EXTENSION
+            else:
+              extension = TRAIN_EXTENSION
+
+            f = file(image_path)
+            file_content = f.read()
+
+            write_artist_tfrecord(artist_dir, artist_number, image_number, filename, extension, file_content, width, height)
+            image_number += 1
+
+    return image_number > 0
+
+def write_artist_tfrecord(artist_dir, artist_number, image_number, filename, extension, file_content, width, height):
   output_path = os.path.join(TARGET_DIR, '{}_{}.{}'.format(artist_dir, image_number, extension))
 
   print "writing tfrecord for artist {}, label {} and image {} into {}".format(artist_dir, artist_number, image_number, output_path)
   writer = tf.python_io.TFRecordWriter(output_path)
-  example = convert_to_example(filename, image, artist_number)
+  example = convert_to_example(filename, file_content, artist_number, width, height)
   writer.write(example.SerializeToString())
   writer.close()
 
-def convert_to_example(filename, image, label):
+def convert_to_example(filename, file_content, label, width, height):
   return tf.train.Example(features=tf.train.Features(feature={
-      'image/height': _int64_feature(HEIGHT),
-      'image/width': _int64_feature(WIDTH),
+      'image/height': _int64_feature(height),
+      'image/width': _int64_feature(width),
       'image/channels': _int64_feature(CHANNELS),
       'image/label': _int64_feature(label),
       'image/filename': _bytes_feature(tf.compat.as_bytes(filename)),
-      'image/encoded': _bytes_feature(tf.compat.as_bytes(image))}))
+      'image/encoded': _bytes_feature(tf.compat.as_bytes(file_content))}))
 
 def _int64_feature(value):
   if not isinstance(value, list): value = [value]
--- network/train.py
+++ network/train.py
@@ -21,62 +21,70 @@ def apply_with_random_selector(x, func, num_cases):
       for case in range(num_cases)])[0]
 
 def distort_color(image, color_ordering=0):
-  if color_ordering == 0:
-    image = tf.image.random_brightness(image, max_delta=32. / 255.)
-    image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
-    image = tf.image.random_hue(image, max_delta=0.05)
-    image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
-  elif color_ordering == 1:
-    image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
-    image = tf.image.random_brightness(image, max_delta=32. / 255.)
-    image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
-    image = tf.image.random_hue(image, max_delta=0.05)
-  elif color_ordering == 2:
-    image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
-    image = tf.image.random_hue(image, max_delta=0.05)
-    image = tf.image.random_brightness(image, max_delta=32. / 255.)
-    image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
-  elif color_ordering == 3:
-    image = tf.image.random_hue(image, max_delta=0.05)
-    image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
-    image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
-    image = tf.image.random_brightness(image, max_delta=32. / 255.)
-  else:
-    raise ValueError('color_ordering must be in [0, 3]')
+  with tf.name_scope('distort_color'):
+    if color_ordering == 0:
+      image = tf.image.random_brightness(image, max_delta=0.25)
+      image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
+      image = tf.image.random_hue(image, max_delta=0.05)
+      image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
+    elif color_ordering == 1:
+      image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
+      image = tf.image.random_brightness(image, max_delta=0.25)
+      image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
+      image = tf.image.random_hue(image, max_delta=0.05)
+    elif color_ordering == 2:
+      image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
+      image = tf.image.random_hue(image, max_delta=0.05)
+      image = tf.image.random_brightness(image, max_delta=0.25)
+      image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
+    elif color_ordering == 3:
+      image = tf.image.random_hue(image, max_delta=0.05)
+      image = tf.image.random_saturation(image, lower=0.75, upper=1.25)
+      image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
+      image = tf.image.random_brightness(image, max_delta=0.25)
+    else:
+      raise ValueError('color_ordering must be in [0, 3]')
 
-  return tf.clip_by_value(image, 0.0, 1.0)
+    return tf.clip_by_value(image, 0.0, 1.0)
 
-def distort_image(image):
-  image = apply_with_random_selector(image, lambda x, ordering: distort_color(x, ordering), num_cases=4)
+def distort_image(image, height, width):
+  with tf.name_scope('distort_image'):
+    image = apply_with_random_selector(image, lambda x, ordering: distort_color(x, ordering), num_cases=4)
 
-  rand = tf.random_uniform([], maxval=0.2, dtype=tf.float32) - 0.1
-  image = tf.contrib.image.transform(
-      image,
-      [1, rand, rand, rand, 1, rand, 0, 0])
+    rand = tf.random_uniform([], maxval=0.2, dtype=tf.float32) - 0.1
+    image = tf.contrib.image.transform(image, [1, rand, rand, rand, 1, rand, 0, 0])
 
-  height_offset = tf.random_uniform([], maxval=0.4, dtype=tf.float32) - 0.2
-  width_offset = tf.random_uniform([], maxval=0.4, dtype=tf.float32) - 0.2
-  image = tf.image.extract_glimpse(
-    tf.expand_dims(image, 0),
-    [HEIGHT, WIDTH],
-    tf.expand_dims([height_offset, width_offset], 0),
-    normalized=True,
-    centered=True)
+    glimpse_min_height = tf.cast(tf.multiply(tf.cast(height, tf.float32), tf.constant(0.8)), tf.int64)
+    glimpse_min_width = tf.cast(tf.multiply(tf.cast(width, tf.float32), tf.constant(0.8)), tf.int64)
 
-  return image
+    glimpse_height = tf.cast(tf.random_uniform([], minval=glimpse_min_height, maxval=height, dtype=tf.int64), tf.int32)
+    glimpse_width = tf.cast(tf.random_uniform([], minval=glimpse_min_width, maxval=width, dtype=tf.int64), tf.int32)
+
+    height_offset = tf.random_uniform([], minval=-0.2, maxval=0.2, dtype=tf.float32)
+    width_offset = tf.random_uniform([], minval=-0.2, maxval=0.2, dtype=tf.float32)
+
+    image = tf.image.extract_glimpse(
+      tf.expand_dims(image, 0),
+      size=[glimpse_height, glimpse_width],
+      offsets=tf.expand_dims([height_offset, width_offset], 0),
+      normalized=True,
+      centered=True)
+
+    return image
 
 def input_batch(mode):
-  if mode == learn.ModeKeys.TRAIN:
-    pattern = os.path.join(TRAIN_DIR, '*.train')
-  else:
-    pattern = os.path.join(TRAIN_DIR, '*.validation')
+  with tf.name_scope('input_batch'):
+    if mode == learn.ModeKeys.TRAIN:
+      pattern = os.path.join(TRAIN_DIR, '*.train')
+    else:
+      pattern = os.path.join(TRAIN_DIR, '*.validation')
 
-  filenames = tf.train.match_filenames_once(pattern)
-  filename_queue = tf.train.string_input_producer(filenames, name="filename_queue")
+    filenames = tf.train.match_filenames_once(pattern)
+    filename_queue = tf.train.string_input_producer(filenames, name="filename_queue")
 
-  reader = tf.TFRecordReader()
-  _, serialized_example = reader.read(filename_queue)
-  features = tf.parse_single_example(
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(
       serialized_example,
         features={
           'image/height': tf.FixedLenFeature([], tf.int64),
@@ -87,28 +95,28 @@ def input_batch(mode):
           'image/encoded': tf.FixedLenFeature([], tf.string)
       })
 
-  height = features['image/height']
-  width = features['image/width']
-  label = features['image/label']
-  image_jpeg = features['image/encoded']
+    height = features['image/height']
+    width = features['image/width']
+    label = features['image/label']
+    image_jpeg = features['image/encoded']
 
-  image = decode_jpeg(image_jpeg)
-  image = resize_image(image)
-  image = distort_image(image)
+    image = decode_jpeg(image_jpeg)
+    image = distort_image(image, height, width)
+    image = resize_image(image)
 
-  num_threads = 4
-  min_after_dequeue = BATCH_SIZE * 4
-  capacity = min_after_dequeue + num_threads * BATCH_SIZE
+    num_threads = 4
+    min_after_dequeue = BATCH_SIZE * 4
+    capacity = min_after_dequeue + num_threads * BATCH_SIZE
 
-  image_batch, label_batch = tf.train.shuffle_batch(
+    image_batch, label_batch = tf.train.shuffle_batch(
       [image, label],
       num_threads=num_threads,
       batch_size=BATCH_SIZE,
       min_after_dequeue=min_after_dequeue,
       capacity=capacity)
 
-  #label_batch = tf.Print(label_batch, [label_batch], message="labels: ", summarize=BATCH_SIZE)
-  return image_batch, label_batch
+    #label_batch = tf.Print(label_batch, [label_batch], message="labels: ", summarize=BATCH_SIZE)
+    return image_batch, label_batch
 
 
 #tf.logging.set_verbosity(tf.logging.INFO)
